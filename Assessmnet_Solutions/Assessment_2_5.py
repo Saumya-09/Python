c7fe24c13a9c2fb00861531a6d148d7005459fc5
achal bhatt <bhatt.saumya.09@gmail.com>
2020-06-20 18:48:59 +0530

    Add : Added files

--- Assessmnet_Solutions/Assessment_2_5.py
+++
@@ -1,8 +0,0 @@
-def Chkprime(n):
-    for i in range(2,n):
-        if n%i==0:
-            return ("Not Prime")
-        else:
-            return ("Prime")
-x=int(input())
-print(Chkprime(x))
--- Assessmnet_Solutions/Assessment_5_4.py
+++
@@ -1,9 +0,0 @@
-def rec(x):
-    if x>0:
-        n=x//10
-        temp=(x%10)+rec(n)
-    else:
-        return 0
-    return temp
-x=int(input())
-print(rec(x))
